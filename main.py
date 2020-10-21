a7f3f360bee7537c97088870b6419652daf43a17
andrea abed <andrea.abed@yahoo.com>
2020-10-21 15:35:23 -0700

    named it travel

--- main.py
+++ main.py
@@ -8,25 +8,16 @@ app = Flask(__name__)
 #connects default URL of server to render home.html
 @app.route('/')
 def home_route():
-    return render_template("home.html", projects=projects.setup())
+    return render_template("travel.html", projects=projects.setup())
 
-#connects /hello path of server to render hello.html
-@app.route('/hello/')
-def hello_rooute():
-    return render_template("hello.html", projects=projects.setup())
 
-#connects /flask path of server to render flask.html
-@app.route('/flask/')
-def flask_route():
-    return render_template("flask.html", projects=projects.setup())
+@app.route("/greece/")
+def greece_route():
+    return "<h1 style='background-color:blue;color:white'>Greece!</h1>"
 
-@app.route("/playground/")
-def playground_route():
-    return render_template("playground.html")
-
-@app.route("/heyheyhey/")
-def heyheyhey_route():
-    return "<h1 style='background-color:blue;color:white'>Hey Hey Hey!</h1>"
+@app.route("/italy/")
+def italy_route():
+    return "<h1 style='background-color:blue;color:white'>Italy!</h1>"
 
 if __name__ == "__main__":
     #runs the application on the repl development server
